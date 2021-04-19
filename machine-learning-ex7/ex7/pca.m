06a4b85dda21b08c25ab8eed3a2a1a136de0c2fe
Boonichi <52070442+Boonichi@users.noreply.github.com>
2021-04-19 16:51:32 +0700

    Update

--- machine-learning-ex7/ex7/pca.m
+++ machine-learning-ex7/ex7/pca.m
@@ -19,7 +19,8 @@ S = zeros(n);
 % Note: When computing the covariance matrix, remember to divide by m (the
 %       number of examples).
 %
-[U,S,V] = svd((X'*X)./(size(X,2)))
+Sigma = (1/m)*(X'*X);
+[U,S,V] = svd(Sigma);
 
 
 
